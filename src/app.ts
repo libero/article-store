ba611d58828bcde1953c1a1c2083a8816727fc12
Chris Wilkinson <c.wilkinson@elifesciences.org>
2019-11-06 14:37:37 +0000

    CS fixes

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,10 @@
-import Koa, {Context} from 'koa';
-import {constants as status_codes} from 'http2';
+import Koa, { Context } from 'koa';
+import { constants as statusCodes } from 'http2';
 
 const app = new Koa();
 
-app.use(async ({response}: Context): Promise<void> => {
-  response.status = status_codes.HTTP_STATUS_OK;
+app.use(async ({ response }: Context): Promise<void> => {
+  response.status = statusCodes.HTTP_STATUS_OK;
 });
 
 export default app;
