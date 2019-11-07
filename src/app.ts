149ea0795fb4b7b738835ddbd30540fb8e51d312
Chris Wilkinson <c.wilkinson@elifesciences.org>
2019-11-07 14:59:41 +0000

    Add request/response logging

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,10 @@
 import Koa, { Context } from 'koa';
+import logger from 'koa-logger';
 import { constants as statusCodes } from 'http2';
 
 const app = new Koa();
 
+app.use(logger());
 app.use(async ({ response }: Context): Promise<void> => {
   response.status = statusCodes.HTTP_STATUS_OK;
 });
