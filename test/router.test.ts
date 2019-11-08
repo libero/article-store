74b58fdb1ade53b598b792b5a6e6b08101147637
Chris Wilkinson <c.wilkinson@elifesciences.org>
2019-11-08 13:30:33 +0000

    Reword

--- test/router.test.ts
+++ test/router.test.ts
@@ -1,11 +1,10 @@
-import indefinite from 'indefinite';
 import createRouter, { Routes } from '../src/router';
 
 describe('router', (): void => {
   const router = createRouter();
 
   Object.entries(Routes).forEach(([key, value]) => {
-    it(`has ${indefinite(key)} route`, (): void => {
+    it(`has a route named ${key}`, (): void => {
       expect(router.route(value)).toBeTruthy();
     });
   });
