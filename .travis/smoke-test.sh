5ee7e3e3252ca17ba7ae3d2d81912910545ff9c9
Chris Wilkinson <c.wilkinson@elifesciences.org>
2019-11-11 15:41:22 +0000

    Use lines rather than characters

--- .travis/smoke-test.sh
+++ .travis/smoke-test.sh
@@ -9,9 +9,9 @@ trap finish EXIT
 
 make start wait-healthy
 
-output=$(curl --silent --show-error --write-out '%{http_code}' 'http://localhost:8080/')
-body=${output::${#output}-3}
-status=${output: -3}
+output=$(curl --silent --show-error --write-out '\n%{http_code}' 'http://localhost:8080/')
+body=$(echo "$output" | head --lines=-1)
+status=$(echo "$output" | tail --lines=1)
 
 [[ "$status" == 200 ]]
 jq --exit-status "$body" /dev/null
