2eb1fd188f016bde6d106a8693dc27519dc76a81
Chris Wilkinson <c.wilkinson@elifesciences.org>
2019-11-11 11:58:56 +0000

    Simpler

--- src/routes/entry-point.ts
+++ src/routes/entry-point.ts
@@ -1,5 +1,4 @@
 import Router from '@koa/router';
-import http from 'http2';
 import { Context, Middleware, Next } from 'koa';
 import { schema } from 'rdf-namespaces';
 import url from 'url';
@@ -7,13 +6,12 @@ import Routes from './index';
 
 export default (router: Router): Middleware => (
   async ({ request, response }: Context, next: Next): Promise<void> => {
-    response.status = http.constants.HTTP_STATUS_OK;
     response.body = {
       '@id': url.resolve(request.origin, router.url(Routes.EntryPoint, {})),
       '@type': schema.EntryPoint,
       [schema.name]: { '@value': 'Article Store', '@language': 'en' },
     };
-    response.type = 'application/ld+json';
+    response.type = 'jsonld';
 
     await next();
   }
