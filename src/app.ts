5f9acecf7f288ed82d9fe07a5140a72dbefef14a
Chris Wilkinson <c.wilkinson@elifesciences.org>
2019-11-14 16:13:31 +0000

    Return errors in Hydra format

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import cors from '@koa/cors';
 import Koa from 'koa';
 import logger from 'koa-logger';
+import errorHandler from './middleware/error-handler';
 import routing from './middleware/routing';
 import createRouter from './router';
 
@@ -9,6 +10,7 @@ const router = createRouter();
 
 app.use(logger());
 app.use(cors());
+app.use(errorHandler());
 app.use(routing(router));
 
 export default app;
---
+++ src/middleware/error-handler.ts
@@ -0,0 +1,33 @@
+import createError, { HttpError } from 'http-errors';
+import { Context, Middleware, Next } from 'koa';
+import { hydra } from 'rdf-namespaces';
+
+const handleHttpError = (error: HttpError, { response }: Context): void => {
+  response.status = error.status;
+  response.body = {
+    '@type': hydra.Status,
+    [hydra.title]: { '@value': response.message, '@language': 'en' },
+  };
+
+  if (error.message !== response.message) {
+    response.body[hydra.description] = { '@value': error.message, '@language': 'en' };
+  }
+
+  response.type = 'jsonld';
+};
+
+const toHttpError = (error: unknown): HttpError => (
+  error instanceof HttpError ? error : createError(error)
+);
+
+export default (): Middleware => (
+  async (context: Context, next: Next): Promise<void> => {
+    try {
+      await next();
+    } catch (error) {
+      handleHttpError(toHttpError(error), context);
+
+      context.app.emit('error', error, context);
+    }
+  }
+);
