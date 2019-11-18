8d0ea4c35100b735331cf64edd80f94b8abb0f8a
Chris Wilkinson <c.wilkinson@elifesciences.org>
2019-11-18 09:31:51 +0000

    Simplify

--- test/middleware/api-documentation.test.ts
+++ test/middleware/api-documentation.test.ts
@@ -14,12 +14,13 @@ describe('API documentation middleware', (): void => {
   it('adds the API documentation link', async (): Promise<void> => {
     const response = await makeRequest();
 
-    const links = parseLinkHeader(response.get('Link'));
+    const expected = {
+      'http://www.w3.org/ns/hydra/core#apiDocumentation': {
+        rel: 'http://www.w3.org/ns/hydra/core#apiDocumentation',
+        url: 'http://example.com/path-to/api-documentation',
+      },
+    };
 
-    expect(Object.keys(links)).toHaveLength(1);
-
-    const link = links['http://www.w3.org/ns/hydra/core#apiDocumentation'];
-
-    expect(link.url).toBe('http://example.com/path-to/api-documentation');
+    expect(parseLinkHeader(response.get('Link'))).toStrictEqual(expected);
   });
 });
