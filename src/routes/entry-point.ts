4e140ef760ab87ba0b6bc28b5cadc080805ea167
Chris Wilkinson <c.wilkinson@elifesciences.org>
2019-11-18 11:33:51 +0000

    Start simpler

--- src/routes/entry-point.ts
+++ src/routes/entry-point.ts
@@ -1,6 +1,6 @@
 import Router from '@koa/router';
 import { Context, Middleware, Next } from 'koa';
-import { hydra, rdf, schema } from 'rdf-namespaces';
+import { hydra, schema } from 'rdf-namespaces';
 import Routes from './index';
 
 export default (router: Router): Middleware => (
@@ -12,15 +12,7 @@ export default (router: Router): Middleware => (
       '@id': router.url(Routes.EntryPoint),
       '@type': schema.EntryPoint,
       [schema.name]: { '@value': 'Article Store', '@language': 'en' },
-      [hydra.collection]: {
-        '@id': router.url(Routes.ArticleList),
-        '@type': hydra.Collection,
-        [hydra.title]: { '@value': 'List of articles', '@language': 'en' },
-        'http://www.w3.org/ns/hydra/core#manages': {
-          'http://www.w3.org/ns/hydra/core#property': rdf.type,
-          'http://www.w3.org/ns/hydra/core#object': schema.Article,
-        },
-      },
+      [hydra.collection]: { '@id': router.url(Routes.ArticleList) },
     };
     response.type = 'jsonld';
 
