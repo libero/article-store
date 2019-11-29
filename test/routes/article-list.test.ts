17ed861962fe4b5eb5565b99529e5bb73e120ab2
nlisgo <nathan@aeopus.com>
2019-11-29 11:35:42 +0000

    Expand article list tests to include empty and number or results

--- test/routes/article-list.test.ts
+++ test/routes/article-list.test.ts
@@ -5,23 +5,45 @@ import createContext from '../context';
 import runMiddleware from '../middleware';
 import InMemoryArticles from '../../src/adaptors/in-memory-articles';
 
-const makeRequest = async (next?: Next): Promise<Response> => {
+const makeRequestEmptyLst = async (next?: Next): Promise<Response> => {
   const context = createContext();
   const articles = new InMemoryArticles();
 
   return runMiddleware(articleList(articles, context.router), context, next);
 };
 
+const makeRequestList = async (next?: Next): Promise<Response> => {
+  const context = createContext();
+  const articles = new InMemoryArticles();
+
+  (async (): Promise<void> => {
+    await Promise.all([
+      articles.add({
+        '@type': 'http://schema.org/Article',
+        '@id': '/articles/09560',
+        'http://schema.org/name': 'Homo naledi, a new species of the genus Homo from the Dinaledi Chamber, South Africa',
+      }),
+      articles.add({
+        '@type': 'http://schema.org/Article',
+        '@id': '/articles/24231',
+        'http://schema.org/name': 'The age of Homo naledi and associated sediments in the Rising Star Cave, South Africa',
+      }),
+    ]);
+  })();
+
+  return runMiddleware(articleList(articles, context.router), context, next);
+};
+
 describe('article list', (): void => {
   it('should return a successful response', async (): Promise<void> => {
-    const response = await makeRequest();
+    const response = await makeRequestEmptyLst();
 
     expect(response.status).toBe(200);
     expect(response.type).toBe('application/ld+json');
   });
 
-  it('should return the list', async (): Promise<void> => {
-    const response = await makeRequest();
+  it('should return an empty list', async (): Promise<void> => {
+    const response = await makeRequestEmptyLst();
     const graph = await jsonld.expand(response.body);
 
     expect(graph).toHaveLength(1);
@@ -33,12 +55,32 @@ describe('article list', (): void => {
     expect(object).toHaveProperty(['http://www.w3.org/ns/hydra/core#title']);
     expect(object).toHaveProperty(['http://www.w3.org/ns/hydra/core#manages']);
     expect(object).toHaveProperty(['http://www.w3.org/ns/hydra/core#totalItems']);
+    expect(object['http://www.w3.org/ns/hydra/core#totalItems'][0]['@value']).toEqual(0);
     expect(object).toHaveProperty(['http://www.w3.org/ns/hydra/core#member']);
+    expect(object['http://www.w3.org/ns/hydra/core#member'][0]['@list']).toHaveLength(0);
+  });
+
+  it('should return the list', async (): Promise<void> => {
+    const response = await makeRequestList();
+    const graph = await jsonld.expand(response.body);
+
+    expect(graph).toHaveLength(1);
+
+    const object = graph[0];
+
+    expect(object['@id']).toBe('http://example.com/path-to/article-list');
+    expect(object['@type']).toContain('http://www.w3.org/ns/hydra/core#Collection');
+    expect(object).toHaveProperty(['http://www.w3.org/ns/hydra/core#title']);
+    expect(object).toHaveProperty(['http://www.w3.org/ns/hydra/core#manages']);
+    expect(object).toHaveProperty(['http://www.w3.org/ns/hydra/core#totalItems']);
+    expect(object['http://www.w3.org/ns/hydra/core#totalItems'][0]['@value']).toEqual(2);
+    expect(object).toHaveProperty(['http://www.w3.org/ns/hydra/core#member']);
+    expect(object['http://www.w3.org/ns/hydra/core#member'][0]['@list']).toHaveLength(2);
   });
 
   it('should call the next middleware', async (): Promise<void> => {
     const next = jest.fn();
-    await makeRequest(next);
+    await makeRequestEmptyLst(next);
 
     expect(next).toHaveBeenCalledTimes(1);
   });
