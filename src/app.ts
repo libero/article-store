f188b6bcce37099c75717f16c279a218e14d23d6
Chris Wilkinson <c.wilkinson@elifesciences.org>
2019-12-02 15:31:42 +0000

    Not meant

--- src/app.ts
+++ src/app.ts
@@ -2,27 +2,16 @@ import cors from '@koa/cors';
 import Koa from 'koa';
 import logger from 'koa-logger';
 import { DataFactory, Store as N3Store } from 'n3';
-import { Quad, Store } from 'rdf-js';
-import streamArray from 'stream-array';
+import { Store } from 'rdf-js';
 import apiDocumentation from './middleware/api-documentation';
 import errorHandler from './middleware/error-handler';
 import jsonld from './middleware/jsonld';
 import routing from './middleware/routing';
-import { rdf, schema } from './namespaces';
 import createRouter from './router';
 
 const app = new Koa();
 const articles: Store = new N3Store();
 const router = createRouter(articles, DataFactory);
-
-function* createArticle(): Generator<Quad> {
-  const articleId = DataFactory.blankNode();
-
-  yield DataFactory.quad(articleId, rdf('type'), schema('Article'), articleId);
-  yield DataFactory.quad(articleId, schema('name'), DataFactory.literal(`Article ${articleId.value}`, 'en'), articleId);
-}
-
-articles.import(streamArray([...createArticle(), ...createArticle(), ...createArticle()]));
 
 app.use(logger());
 app.use(cors({
