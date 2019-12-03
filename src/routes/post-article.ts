24b6898e1bdbb77248799486bca7428f509faec3
nlisgo <nathan@aeopus.com>
2019-12-03 14:44:43 +0000

    Handle failure to add article

--- src/routes/post-article.ts
+++ src/routes/post-article.ts
@@ -1,10 +1,18 @@
 import { constants } from 'http2';
 import { Context, Middleware, Next } from 'koa';
+import { JsonLdObj } from 'jsonld/jsonld-spec';
+import createHttpError from 'http-errors';
 import Articles from '../articles';
 
 export default (articles: Articles): Middleware => (
   async ({ request, response }: Context, next: Next): Promise<void> => {
-    articles.add(request.body);
+    const article = request.body as JsonLdObj;
+    try {
+      await articles.add(article);
+    } catch {
+      throw new createHttpError.NotImplemented();
+    }
+
     response.status = constants.HTTP_STATUS_NO_CONTENT;
     response.type = 'jsonld';
 
