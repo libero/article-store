b0cac705c5faf9c94d2ecf07a652679bff545134
nlisgo <nathan@aeopus.com>
2019-12-05 11:53:53 +0000

    Change ArticleHasNoID to ArticleHasNoId for consistency

--- test/errors/article-has-no-id.test.ts
+++ test/errors/article-has-no-id.test.ts
@@ -1,14 +1,14 @@
-import ArticleHasNoID from '../../src/errors/article-has-no-id';
+import ArticleHasNoId from '../../src/errors/article-has-no-id';
 
 describe('article has no ID error', (): void => {
   it('should be an error', async (): Promise<void> => {
-    const error = new ArticleHasNoID();
+    const error = new ArticleHasNoId();
 
     expect(error).toBeInstanceOf(Error);
   });
 
   it('should have a message', async (): Promise<void> => {
-    const error = new ArticleHasNoID();
+    const error = new ArticleHasNoId();
 
     expect(error.message).toBe('Article must have an ID');
   });
