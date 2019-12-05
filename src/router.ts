25edc32f44b49610101978449268ded4abaa186b
Nathan Lisgo <nathan@aeopus.com>
2019-12-05 14:20:06 +0000

    feat: add article operation
    Allow articles to be added to the article list.

--- src/router.ts
+++ src/router.ts
@@ -1,6 +1,7 @@
 import Router from '@koa/router';
 import Articles from './articles';
 import Routes from './routes';
+import addArticle from './routes/add-article';
 import apiDocumentation from './routes/api-documentation';
 import articleList from './routes/article-list';
 import entryPoint from './routes/entry-point';
@@ -10,6 +11,7 @@ export default (articles: Articles): Router => {
 
   router.get(Routes.ApiDocumentation, '/doc', apiDocumentation(router));
   router.get(Routes.ArticleList, '/articles', articleList(articles, router));
+  router.post(Routes.AddArticle, '/articles', addArticle(articles));
   router.get(Routes.EntryPoint, '/', entryPoint(router));
 
   return router;
