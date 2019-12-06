1bb121740482b2573223af200c6c621d8388eb8a
Chris Wilkinson <c.wilkinson@elifesciences.org>
2019-12-06 13:51:08 +0000

    refactor: separate API documentation link middleware from the router
    Reduces coupling, as the path doesn't have to be recomputed.

--- src/app.ts
+++ src/app.ts
@@ -7,6 +7,7 @@ import apiDocumentation from './middleware/api-documentation';
 import errorHandler from './middleware/error-handler';
 import routing from './middleware/routing';
 import createRouter from './router';
+import Routes from './routes';
 
 export default (articles: Articles): Koa => {
   const app = new Koa();
@@ -21,7 +22,7 @@ export default (articles: Articles): Koa => {
   app.use(cors({
     exposeHeaders: ['Link'],
   }));
-  app.use(apiDocumentation(router));
+  app.use(apiDocumentation(router.url(Routes.ApiDocumentation)));
   app.use(errorHandler());
   app.use(routing(router));
 
