2a622fff8f526e776a37fd40432bf037ae3dd5a4
nlisgo <nathan@aeopus.com>
2019-12-10 14:51:44 +0000

    Treat NotAnArticle as a Bad Request

--- src/middleware/error-handler.ts
+++ src/middleware/error-handler.ts
@@ -1,6 +1,8 @@
 import createError, { HttpError, UnknownError } from 'http-errors';
 import { Context, Middleware, Next } from 'koa';
 import { hydra } from 'rdf-namespaces';
+import NotAnArticle from '../errors/not-an-article';
+import createHttpError from 'http-errors';
 
 const handleHttpError = (error: HttpError, { response }: Context): void => {
   response.status = error.status;
@@ -16,9 +18,12 @@ const handleHttpError = (error: HttpError, { response }: Context): void => {
   response.type = 'jsonld';
 };
 
-const toHttpError = (error: UnknownError): HttpError => (
-  error instanceof HttpError ? error : createError(error)
-);
+const toHttpError = (error: UnknownError): HttpError => {
+  if (error instanceof NotAnArticle) {
+    error = new createHttpError.BadRequest(error.message);
+  }
+  return error instanceof HttpError ? error : createError(error);
+};
 
 export default (): Middleware => (
   async (context: Context, next: Next): Promise<void> => {
