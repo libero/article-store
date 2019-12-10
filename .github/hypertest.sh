d2b650188115f3ae89f12220f7493832762a0a38
Chris Wilkinson <c.wilkinson@elifesciences.org>
2019-12-10 13:02:25 +0000

    Use --init for now

--- .github/hypertest.sh
+++ .github/hypertest.sh
@@ -9,4 +9,4 @@ trap finish EXIT
 
 make start wait-healthy
 
-docker run --rm --network host --mount "type=bind,source=$(pwd)/test/hypertest/,destination=/tests" hydrofoil/hypertest:latest --baseUri http://localhost:8080/
+docker run --rm --init --network host --mount "type=bind,source=$(pwd)/test/hypertest/,destination=/tests" hydrofoil/hypertest:latest --baseUri http://localhost:8080/
