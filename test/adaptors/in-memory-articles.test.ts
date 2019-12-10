ba9e779a20ebe2b8bd965a7396f9bf829a2c52a9
Nathan Lisgo <nathan@aeopus.com>
2019-12-10 15:00:00 +0000

    test: don't use Jest's toThrow matcher
    Currently the error message is being checked rather than the type, this introduces more specific matches.

--- test/adaptors/in-memory-articles.test.ts
+++ test/adaptors/in-memory-articles.test.ts
@@ -27,7 +27,7 @@ describe('in-memory articles', (): void => {
   it('throws an error if the article does not have an ID', async (): Promise<void> => {
     const articles = new InMemoryArticles();
 
-    await expect(articles.add({})).rejects.toThrow(new ArticleHasNoId());
+    await expect(articles.add({})).rejects.toBeInstanceOf(ArticleHasNoId);
   });
 
   it('can retrieve an article', async (): Promise<void> => {
@@ -41,7 +41,8 @@ describe('in-memory articles', (): void => {
   it('throws an error if the article is not found', async (): Promise<void> => {
     const articles = new InMemoryArticles();
 
-    await expect(articles.get('_:1')).rejects.toThrow(new ArticleNotFound('_:1'));
+    await expect(articles.get('_:1')).rejects.toBeInstanceOf(ArticleNotFound);
+    await expect(articles.get('_:1')).rejects.toHaveProperty('id', '_:1');
   });
 
   it('can remove an article', async (): Promise<void> => {
