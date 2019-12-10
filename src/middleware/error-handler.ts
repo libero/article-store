2892d09e0041c8be3aae7758d766bb1e926d11ea
nlisgo <nathan@aeopus.com>
2019-12-10 15:14:25 +0000

    Revert "Treat NotAnArticle as a Bad Request"
    This reverts commit 2a622fff8f526e776a37fd40432bf037ae3dd5a4.

--- src/middleware/error-handler.ts
+++ src/middleware/error-handler.ts
@@ -1,8 +1,6 @@
 import createError, { HttpError, UnknownError } from 'http-errors';
 import { Context, Middleware, Next } from 'koa';
 import { hydra } from 'rdf-namespaces';
-import NotAnArticle from '../errors/not-an-article';
-import createHttpError from 'http-errors';
 
 const handleHttpError = (error: HttpError, { response }: Context): void => {
   response.status = error.status;
@@ -18,12 +16,9 @@ const handleHttpError = (error: HttpError, { response }: Context): void => {
   response.type = 'jsonld';
 };
 
-const toHttpError = (error: UnknownError): HttpError => {
-  if (error instanceof NotAnArticle) {
-    error = new createHttpError.BadRequest(error.message);
-  }
-  return error instanceof HttpError ? error : createError(error);
-};
+const toHttpError = (error: UnknownError): HttpError => (
+  error instanceof HttpError ? error : createError(error)
+);
 
 export default (): Middleware => (
   async (context: Context, next: Next): Promise<void> => {
