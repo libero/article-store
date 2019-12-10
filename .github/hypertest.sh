b61a2734728f846c0b8d40e0b59a077150a64de2
Chris Wilkinson <c.wilkinson@elifesciences.org>
2019-12-10 12:52:32 +0000

    Use latest

--- .github/hypertest.sh
+++ .github/hypertest.sh
@@ -9,4 +9,4 @@ trap finish EXIT
 
 make start wait-healthy
 
-docker run --rm --network host --mount "type=bind,source=$(pwd)/test/hypertest/,destination=/tests" hydrofoil/hypertest:_0.3.2 --baseUri http://localhost:8080/
+docker run --rm --network host --mount "type=bind,source=$(pwd)/test/hypertest/,destination=/tests" hydrofoil/hypertest:latest --baseUri http://localhost:8080/
