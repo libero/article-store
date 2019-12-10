9083ac19263bc3bc0717a9a968007c44b4d4dfd6
Chris Wilkinson <c.wilkinson@elifesciences.org>
2019-12-10 10:12:14 +0000

    Add types

--- src/errors/not-an-article.ts
+++ src/errors/not-an-article.ts
@@ -1,5 +1,11 @@
+import { Iri } from 'jsonld/jsonld-spec';
+
 export default class NotAnArticle extends Error {
-  constructor(types: Array<string> = []) {
+  readonly types: Array<Iri>;
+
+  constructor(types: Array<Iri> = []) {
     super(`Article type must be http://schema.org/Article (${types.length ? `'${types.join(', ')}'` : 'none'} was given)`);
+
+    this.types = types;
   }
 }
--- test/errors/not-an-article.test.ts
+++ test/errors/not-an-article.test.ts
@@ -11,11 +11,13 @@ describe('not an article error', (): void => {
     const error = new NotAnArticle();
 
     expect(error.message).toBe('Article type must be http://schema.org/Article (none was given)');
+    expect(error.types).toHaveLength(0);
   });
 
   it('may have types', async (): Promise<void> => {
     const error = new NotAnArticle(['http://schema.org/NewsArticle', 'http://schema.org/Book']);
 
     expect(error.message).toBe('Article type must be http://schema.org/Article (\'http://schema.org/NewsArticle, http://schema.org/Book\' was given)');
+    expect(error.types).toStrictEqual(['http://schema.org/NewsArticle', 'http://schema.org/Book']);
   });
 });
