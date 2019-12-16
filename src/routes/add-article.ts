053b5947d198ff56c615c3d7552194463a65955f
Chris Wilkinson <c.wilkinson@elifesciences.org>
2019-12-16 13:12:27 +0000

    feat: use 201 Created response rather than 204 No Content
    Currently adding a new article sees a '204 No Content' response. This was temporary as the articles aren't currently separate resources. However, it doesn't tell clients to go back to the collection. This changes the response to '201 Created' and sets the 'Location' header.

--- src/routes/add-article.ts
+++ src/routes/add-article.ts
@@ -5,11 +5,15 @@ import { JsonLdArray } from 'jsonld/jsonld-spec';
 import { Next } from 'koa';
 import { schema } from 'rdf-namespaces';
 import uniqueString from 'unique-string';
+import url from 'url';
 import { AppContext, AppMiddleware } from '../app';
 import NotAnArticle from '../errors/not-an-article';
+import Routes from './index';
 
 export default (): AppMiddleware => (
-  async ({ articles, request, response }: AppContext, next: Next): Promise<void> => {
+  async ({
+    articles, request, response, router,
+  }: AppContext, next: Next): Promise<void> => {
     const [article] = await jsonld.expand(request.body) as JsonLdArray;
     if ('@id' in article) {
       throw new createHttpError.Forbidden(`Article IDs must not be set ('${article['@id']}' was given)`);
@@ -30,7 +34,8 @@ export default (): AppMiddleware => (
       throw error;
     }
 
-    response.status = constants.HTTP_STATUS_NO_CONTENT;
+    response.status = constants.HTTP_STATUS_CREATED;
+    response.set('Location', url.resolve(request.origin, router.url(Routes.ArticleList)));
 
     await next();
   }
