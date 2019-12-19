34411239684669c33e0ea3a29498fe22572c61f1
Chris Wilkinson <c.wilkinson@elifesciences.org>
2019-12-19 14:05:42 +0000

    Use Clownface

--- src/routes/api-documentation.ts
+++ src/routes/api-documentation.ts
@@ -1,6 +1,7 @@
+import clownface from 'clownface';
 import { constants } from 'http2';
 import { Next } from 'koa';
-import { addAll } from 'rdf-dataset-ext';
+import { NamedNode } from 'rdf-js';
 import { toRdf } from 'rdf-literal';
 import url from 'url';
 import { AppContext, AppMiddleware } from '../app';
@@ -11,129 +12,129 @@ import Routes from './index';
 
 export default (): AppMiddleware => (
   async ({
-    dataFactory: {
-      blankNode, quad, literal, namedNode,
-    }, request, response, router,
+    dataFactory: { literal, namedNode }, request, response, router,
   }: AppContext, next: Next): Promise<void> => {
-    const apiDocumentation = namedNode(
-      url.resolve(request.origin, router.url(Routes.ApiDocumentation)),
-    );
-    const entryPoint = namedNode(url.resolve(request.origin, router.url(Routes.EntryPoint)));
-
-    const quads = [
-      quad(apiDocumentation, rdf.type, hydra.ApiDocumentation),
-      quad(apiDocumentation, hydra.entrypoint, entryPoint),
-    ];
-
-    // properties
-    quads.push(
-      quad(schema('name'), rdf.type, rdf.Property),
-      quad(hydra.manages, rdf.type, rdf.Property),
-      quad(hydra.member, rdf.type, rdf.Property),
-      quad(hydra.member, rdf.type, hydra.Link),
-      quad(hydra.title, rdf.type, rdf.Property),
-      quad(hydra.totalItems, rdf.type, rdf.Property),
+    const createNamedNode = (route: string): NamedNode => (
+      namedNode(url.resolve(request.origin, router.url(route)))
     );
 
-    // schema:EntryPoint
-    const entryPointGet = blankNode();
-    const entryPointName = blankNode();
-    quads.push(
-      quad(apiDocumentation, hydra.supportedClass, schema.EntryPoint),
-      quad(schema.EntryPoint, rdf.type, hydra.Class),
-      quad(schema.EntryPoint, hydra.title, literal('API entry point', 'en')),
-      quad(schema.EntryPoint, hydra.supportedOperation, entryPointGet),
-      quad(entryPointGet, rdf.type, hydra.Operation),
-      quad(entryPointGet, hydra.title, literal('Get the entry point', 'en')),
-      quad(entryPointGet, hydra.method, literal('GET')),
-      quad(entryPointGet, hydra.expects, owl.Nothing),
-      quad(entryPointGet, hydra.returns, schema.EntryPoint),
-      quad(schema.EntryPoint, hydra.supportedProperty, entryPointName),
-      quad(entryPointName, rdf.type, hydra.SupportedProperty),
-      quad(entryPointName, hydra.title, literal('Name', 'en')),
-      quad(entryPointName, hydra.property, schema('name')),
-      quad(entryPointName, hydra.required, toRdf(true)),
-      quad(entryPointName, hydra.readable, toRdf(true)),
-      quad(entryPointName, hydra.writeable, toRdf(false)),
-    );
+    const graph = clownface({
+      dataset: response.dataset,
+      term: createNamedNode(Routes.ApiDocumentation),
+    });
 
-    // schema:Article
-    const articleName = blankNode();
-    quads.push(
-      quad(apiDocumentation, hydra.supportedClass, schema.Article),
-      quad(schema.Article, rdf.type, hydra.Class),
-      quad(schema.Article, hydra.title, literal('Article', 'en')),
-      quad(schema.Article, hydra.supportedProperty, articleName),
-      quad(articleName, rdf.type, hydra.SupportedProperty),
-      quad(articleName, hydra.title, literal('Name', 'en')),
-      quad(articleName, hydra.property, schema('name')),
-      quad(articleName, hydra.required, toRdf(true)),
-      quad(articleName, hydra.readable, toRdf(true)),
-      quad(articleName, hydra.writeable, toRdf(true)),
-    );
+    graph.addOut(rdf.type, hydra.ApiDocumentation);
+    graph.addOut(hydra.entrypoint, createNamedNode(router.url(Routes.EntryPoint)));
 
-    // hydra:Collection
-    const collectionGet = blankNode();
-    const collectionAdd = blankNode();
-    const collectionAdd201 = blankNode();
-    const collectionName = blankNode();
-    const collectionManages = blankNode();
-    const collectionTotalItems = blankNode();
-    const collectionMember = blankNode();
-    quads.push(
-      quad(apiDocumentation, hydra.supportedClass, hydra.Collection),
-      quad(hydra.Collection, rdf.type, hydra.Class),
-      quad(hydra.Collection, hydra.title, literal('Collection', 'en')),
-      quad(hydra.Collection, hydra.supportedOperation, collectionGet),
-      quad(collectionGet, rdf.type, hydra.Operation),
-      quad(collectionGet, hydra.title, literal('Get the collection', 'en')),
-      quad(collectionGet, hydra.method, literal('GET')),
-      quad(collectionGet, hydra.expects, owl.Nothing),
-      quad(collectionGet, hydra.returns, hydra.Collection),
-      quad(hydra.Collection, hydra.supportedOperation, collectionAdd),
-      quad(collectionAdd, rdf.type, hydra.Operation),
-      quad(collectionAdd, hydra.title, literal('Add an article', 'en')),
-      quad(collectionAdd, hydra.method, literal('POST')),
-      quad(collectionAdd, hydra.expects, schema.Article),
-      quad(collectionAdd, hydra.returns, owl.Nothing),
-      quad(collectionAdd, hydra.possibleStatus, collectionAdd201),
-      quad(collectionAdd201, hydra.statusCode, toRdf(constants.HTTP_STATUS_CREATED)),
-      quad(collectionAdd201, hydra.description, literal('If the article was added successfully', 'en')),
-      quad(hydra.Collection, hydra.supportedProperty, collectionName),
-      quad(collectionName, rdf.type, hydra.SupportedProperty),
-      quad(collectionName, hydra.title, literal('Title', 'en')),
-      quad(collectionName, hydra.property, hydra.title),
-      quad(hydra.title, rdf.type, rdf.Property),
-      quad(collectionName, hydra.required, toRdf(true)),
-      quad(collectionName, hydra.readable, toRdf(true)),
-      quad(collectionName, hydra.writeable, toRdf(false)),
-      quad(hydra.Collection, hydra.supportedProperty, collectionManages),
-      quad(collectionManages, rdf.type, hydra.SupportedProperty),
-      quad(collectionManages, hydra.title, literal('What the collection manages', 'en')),
-      quad(collectionManages, hydra.property, hydra.manages),
-      quad(collectionManages, hydra.required, toRdf(true)),
-      quad(collectionManages, hydra.readable, toRdf(true)),
-      quad(collectionManages, hydra.writeable, toRdf(false)),
-      quad(hydra.Collection, hydra.supportedProperty, collectionTotalItems),
-      quad(collectionTotalItems, rdf.type, hydra.SupportedProperty),
-      quad(collectionTotalItems, hydra.title, literal('Total items', 'en')),
-      quad(collectionTotalItems, hydra.property, hydra.totalItems),
-      quad(hydra.totalItems, rdf.type, rdf.Property),
-      quad(collectionTotalItems, hydra.required, toRdf(true)),
-      quad(collectionTotalItems, hydra.readable, toRdf(true)),
-      quad(collectionTotalItems, hydra.writeable, toRdf(false)),
-      quad(hydra.Collection, hydra.supportedProperty, collectionMember),
-      quad(collectionMember, rdf.type, hydra.SupportedProperty),
-      quad(collectionMember, hydra.title, literal('Members of this collection', 'en')),
-      quad(collectionMember, hydra.property, hydra.member),
-      quad(hydra.member, rdf.type, rdf.Property),
-      quad(hydra.member, rdf.type, hydra.Link),
-      quad(collectionMember, hydra.required, toRdf(true)),
-      quad(collectionMember, hydra.readable, toRdf(false)),
-      quad(collectionMember, hydra.writeable, toRdf(false)),
-    );
+    graph.addOut(hydra.supportedClass, schema.EntryPoint, (entryPoint): void => {
+      entryPoint.addOut(rdf.type, hydra.Class);
+      entryPoint.addOut(hydra.title, literal('API entry point', 'en'));
 
-    addAll(response.dataset, quads);
+      entryPoint.addOut(hydra.supportedOperation, (get): void => {
+        get.addOut(rdf.type, hydra.Operation);
+        get.addOut(hydra.title, literal('Get the entry point', 'en'));
+        get.addOut(hydra.method, literal('GET'));
+        get.addOut(hydra.expects, owl.Nothing);
+        get.addOut(hydra.returns, schema.EntryPoint);
+      });
+
+      entryPoint.addOut(hydra.supportedProperty, (name): void => {
+        name.addOut(rdf.type, hydra.SupportedProperty);
+        name.addOut(hydra.title, literal('Name', 'en'));
+        name.addOut(hydra.property, schema('name'), (property): void => {
+          property.addOut(rdf.type, rdf.Property);
+        });
+        name.addOut(hydra.required, true);
+        name.addOut(hydra.readable, true);
+        name.addOut(hydra.writeable, toRdf(false));
+      });
+    });
+
+    graph.addOut(hydra.supportedClass, schema.Article, (article): void => {
+      article.addOut(rdf.type, hydra.Class);
+      article.addOut(hydra.title, literal('Article', 'en'));
+
+      article.addOut(hydra.supportedProperty, (name): void => {
+        name.addOut(rdf.type, hydra.SupportedProperty);
+        name.addOut(hydra.title, literal('Name', 'en'));
+        name.addOut(hydra.property, schema('name'), (property): void => {
+          property.addOut(rdf.type, rdf.Property);
+        });
+        name.addOut(hydra.required, true);
+        name.addOut(hydra.readable, true);
+        name.addOut(hydra.writeable, true);
+      });
+    });
+
+    graph.addOut(hydra.supportedClass, hydra.Collection, (collection): void => {
+      collection.addOut(rdf.type, hydra.Class);
+      collection.addOut(hydra.title, literal('Collection', 'en'));
+
+      collection.addOut(hydra.supportedOperation, (get): void => {
+        get.addOut(rdf.type, hydra.Operation);
+        get.addOut(hydra.title, literal('Get the collection', 'en'));
+        get.addOut(hydra.method, literal('GET'));
+        get.addOut(hydra.expects, owl.Nothing);
+        get.addOut(hydra.returns, hydra.Collection);
+      });
+
+      collection.addOut(hydra.supportedOperation, (add): void => {
+        add.addOut(rdf.type, hydra.Operation);
+        add.addOut(hydra.title, literal('Add an article', 'en'));
+        add.addOut(hydra.method, literal('POST'));
+        add.addOut(hydra.expects, schema.Article);
+        add.addOut(hydra.returns, owl.Nothing);
+        add.addOut(hydra.possibleStatus, (status): void => {
+          status.addOut(hydra.statusCode, constants.HTTP_STATUS_CREATED);
+          status.addOut(hydra.description, literal('If the article was added successfully', 'en'));
+        });
+      });
+
+      collection.addOut(hydra.supportedProperty, (name): void => {
+        name.addOut(rdf.type, hydra.SupportedProperty);
+        name.addOut(hydra.title, literal('Title', 'en'));
+        name.addOut(hydra.property, hydra.title, (property): void => {
+          property.addOut(rdf.type, rdf.Property);
+        });
+        name.addOut(hydra.required, true);
+        name.addOut(hydra.readable, true);
+        name.addOut(hydra.writeable, toRdf(false));
+      });
+
+      collection.addOut(hydra.supportedProperty, (manages): void => {
+        manages.addOut(rdf.type, hydra.SupportedProperty);
+        manages.addOut(hydra.title, literal('What the collection manages', 'en'));
+        manages.addOut(hydra.property, hydra.manages, (property): void => {
+          property.addOut(rdf.type, rdf.Property);
+        });
+        manages.addOut(hydra.required, true);
+        manages.addOut(hydra.readable, true);
+        manages.addOut(hydra.writeable, toRdf(false));
+      });
+
+      collection.addOut(hydra.supportedProperty, (totalItems): void => {
+        totalItems.addOut(rdf.type, hydra.SupportedProperty);
+        totalItems.addOut(hydra.title, literal('Total items', 'en'));
+        totalItems.addOut(hydra.property, hydra.totalItems, (property): void => {
+          property.addOut(rdf.type, rdf.Property);
+        });
+        totalItems.addOut(hydra.required, true);
+        totalItems.addOut(hydra.readable, true);
+        totalItems.addOut(hydra.writeable, toRdf(false));
+      });
+
+      collection.addOut(hydra.supportedProperty, (members): void => {
+        members.addOut(rdf.type, hydra.SupportedProperty);
+        members.addOut(hydra.title, literal('Members of this collection', 'en'));
+        members.addOut(hydra.property, hydra.members, (property): void => {
+          property.addOut(rdf.type, rdf.Property);
+          property.addOut(rdf.type, hydra.Link);
+        });
+        members.addOut(hydra.required, true);
+        members.addOut(hydra.readable, true);
+        members.addOut(hydra.writeable, toRdf(false));
+      });
+    });
 
     response.status = constants.HTTP_STATUS_OK;
 
