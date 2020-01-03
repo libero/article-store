761a7ddd44e7d6371e427c5a4b47c6cf75a1e414
Chris Wilkinson <c.wilkinson@elifesciences.org>
2020-01-03 14:17:12 +0000

    Merge fixes

--- src/errors/not-an-article.ts
+++ src/errors/not-an-article.ts
@@ -1,10 +1,10 @@
-import { Term } from 'rdf-js';
+import { Quad_Object as QuadObject } from 'rdf-js';
 import { termToString } from 'rdf-string';
 
 export default class NotAnArticle extends Error {
-  readonly types: Array<Term>;
+  readonly types: Array<QuadObject>;
 
-  constructor(types: Array<Term> = []) {
+  constructor(types: Array<QuadObject> = []) {
     super(`Article type must be http://schema.org/Article (${types.length ? `'${types.map(termToString).join(', ')}'` : 'none'} was given)`);
 
     this.types = types;
