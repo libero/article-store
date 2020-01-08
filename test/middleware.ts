8d3a9ff3225e620f9fef4bbe4c85058c25c0bc80
Chris Wilkinson <c.wilkinson@elifesciences.org>
2020-01-08 12:00:58 +0000

    refactor: improve request and response typing in tests
    Fixes the request and response types in the context factory, which allows the middleware runner to return the real response type.

--- test/middleware.ts
+++ test/middleware.ts
@@ -1,18 +1,16 @@
-import { Middleware, ParameterizedContext, Response } from 'koa';
-import { AppContext, AppState } from '../src/app';
+import { ExtendableContext } from 'koa';
+import { Middleware } from 'koa-compose';
+import { AppContext } from '../src/app';
 
-export type Next<StateT = AppState, CustomT = AppContext> = (
-  context: ParameterizedContext<StateT, CustomT>
-) => Promise<void>;
+export type Next<CustomT extends ExtendableContext = AppContext> =
+  (context: CustomT) => Promise<void>;
 
-export default async <StateT = AppState, CustomT = AppContext>(
-  middleware: Middleware<StateT, CustomT>,
-  context: ParameterizedContext<StateT, CustomT>,
-  next?: Next<StateT, CustomT>,
-): Promise<Response> => {
+export default async <CustomT extends ExtendableContext>(
+  middleware: Middleware<CustomT>,
+  context: CustomT,
+  next?: Next<CustomT>,
+): Promise<CustomT['response']> => {
   await middleware(context, next ? (): Promise<void> => next(context) : jest.fn());
 
-  const { response } = context;
-
-  return response;
+  return context.response;
 };
