5e9e1796a31b3719b724a63d876de98ad5147f24
Chris Wilkinson <c.wilkinson@elifesciences.org>
2020-01-08 10:45:40 +0000

    Improve Koa types

--- test/app.test.ts
+++ test/app.test.ts
@@ -1,9 +1,8 @@
 import dataFactory from '@rdfjs/data-model';
-import Koa from 'koa';
 import parseLinkHeader from 'parse-link-header';
 import request from 'supertest';
 import InMemoryArticles from '../src/adaptors/in-memory-articles';
-import createApp from '../src/app';
+import createApp, { App } from '../src/app';
 import createRouter from '../src/router';
 import Routes from '../src/routes';
 
@@ -11,7 +10,7 @@ const trim = (value: string): string => value.trim();
 const parseHeader = (header: string): Array<string> => header.split(',').map(trim);
 
 const router = createRouter();
-let app: Koa;
+let app: App;
 
 beforeEach((): void => {
   app = createApp(new InMemoryArticles(), router, router.url(Routes.ApiDocumentation), dataFactory);
