251bec323e5ab392915f4112069d555956bfb9d7
nlisgo <nathan@aeopus.com>
2020-01-08 15:25:30 +0000

    Setup pg-test

--- jest.config.js
+++ jest.config.js
@@ -3,4 +3,6 @@ module.exports = {
     '^.+\\.ts$': 'ts-jest',
   },
   verbose: true,
+  globalSetup: '<rootDir>/node_modules/@databases/pg-test/jest/globalSetup',
+  globalTeardown: '<rootDir>/node_modules/@databases/pg-test/jest/globalTeardown',
 };
--- src/db.ts
+++ src/db.ts
@@ -1,6 +1,8 @@
 import { IConnectionParameters } from 'pg-promise/typescript/pg-subset';
 
-export default {
+export default (process.env.DATABASE_URL) ? {
+  connectionString: process.env.DATABASE_URL,
+} : {
   user: process.env.DATABASE_USER,
   password: process.env.DATABASE_PASSWORD,
   database: process.env.DATABASE_NAME,
