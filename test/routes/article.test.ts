df2865bba55c359acc5002aa6e685d84c2cef2bb
nlisgo <nathan@aeopus.com>
2020-01-22 11:58:08 +0000

    should only attempt article retrieval if next middleware throws not found http error

--- test/routes/article.test.ts
+++ test/routes/article.test.ts
@@ -3,6 +3,7 @@ import {
 } from '@rdfjs/data-model';
 import createHttpError from 'http-errors';
 import { Response } from 'koa';
+import { DatasetCore, NamedNode } from 'rdf-js';
 import InMemoryArticles from '../../src/adaptors/in-memory-articles';
 import Articles from '../../src/articles';
 import article from '../../src/routes/article';
@@ -10,6 +11,7 @@ import createContext from '../context';
 import createArticle from '../create-article';
 import runMiddleware, { NextMiddleware } from '../middleware';
 import { WithDataset } from '../../src/middleware/dataset';
+import ArticleNotFound from '../../src/errors/article-not-found';
 
 const dummyNext = async (): Promise<void> => {
   throw new createHttpError.NotFound();
@@ -33,6 +35,26 @@ describe('article', (): void => {
     const response = await makeRequest(articles, 'path-to/article/one');
 
     expect(response.status).toBe(200);
+  });
+
+  it('should only attempt article retrieval if next middleware throws not found http error', async (): Promise<void> => {
+    const mockArticles: Articles = {
+      set: jest.fn(),
+      get: jest.fn(async (id: NamedNode): Promise<DatasetCore> => {
+        throw new ArticleNotFound(id);
+      }),
+      remove: jest.fn(),
+      contains: jest.fn(),
+      count: jest.fn(),
+      [Symbol.asyncIterator]: jest.fn(),
+    };
+
+    const next = jest.fn();
+    await makeRequest(mockArticles, 'path-to/article/one', next);
+    expect(mockArticles.get).toHaveBeenCalledTimes(0);
+    await expect(makeRequest(mockArticles, 'path-to/article/one')).rejects.toBeInstanceOf(createHttpError.NotFound);
+    expect(mockArticles.get).toHaveBeenCalledTimes(1);
+    expect(mockArticles.get).toHaveBeenCalledWith(namedNode('http://example.com/path-to/article/one'));
   });
 
   it('should throw an error if article is not found', async (): Promise<void> => {
