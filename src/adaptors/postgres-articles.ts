eda0e51a90f9602cced4b36a7a58c706e5d71d9e
nlisgo <nathan@aeopus.com>
2020-01-24 18:40:29 +0000

    Change uuid field to iri

--- src/adaptors/postgres-articles.ts
+++ src/adaptors/postgres-articles.ts
@@ -3,7 +3,6 @@ import { IBaseProtocol, IMain } from 'pg-promise';
 import {
   DatasetCore, NamedNode, Quad, Quad_Object as QuadObject, Sink, Stream,
 } from 'rdf-js';
-import { stringToTerm, termToString } from 'rdf-string';
 import ParserJsonld from '@rdfjs/parser-jsonld';
 import SerializerJsonld from '@rdfjs/serializer-jsonld-ext';
 import pEvent from 'p-event';
@@ -14,6 +13,7 @@ import ArticleNotFound from '../errors/article-not-found';
 import NotAnArticle from '../errors/not-an-article';
 import { rdf, schema } from '../namespaces';
 import { ExtendedDataFactory } from '../middleware/dataset';
+import { namedNode } from '../data-factory';
 
 export default class PostgresArticles implements Articles {
   private database: IBaseProtocol<IMain>;
@@ -38,14 +38,14 @@ export default class PostgresArticles implements Articles {
       throw new NotAnArticle([...article.match(id, rdf.type)].map((quad: Quad): QuadObject => quad.object));
     }
 
-    await this.database.none('INSERT INTO articles(uuid, article) VALUES ($[uuid], $[article]) ON CONFLICT (uuid) DO UPDATE SET article = $[article]', {
-      uuid: termToString(id),
+    await this.database.none('INSERT INTO articles(iri, article) VALUES ($[iri], $[article]) ON CONFLICT (iri) DO UPDATE SET article = $[article]', {
+      iri: id.value,
       article: JSON.stringify(await pEvent(this.serialiser.import(toStream(article)), 'data')),
     });
   }
 
   async get(id: NamedNode): Promise<DatasetCore> {
-    const article = await this.database.oneOrNone('SELECT article FROM articles WHERE uuid = $[uuid]', { uuid: termToString(id) }, (data: { article: string } | null) => (
+    const article = await this.database.oneOrNone('SELECT article FROM articles WHERE iri = $[iri]', { iri: id.value }, (data: { article: string } | null) => (
       (data) ? data.article : data
     ));
 
@@ -57,11 +57,11 @@ export default class PostgresArticles implements Articles {
   }
 
   async remove(id: NamedNode): Promise<void> {
-    await this.database.none('DELETE FROM articles WHERE uuid = $[uuid]', { uuid: termToString(id) });
+    await this.database.none('DELETE FROM articles WHERE iri = $[iri]', { iri: id.value });
   }
 
   async contains(id: NamedNode): Promise<boolean> {
-    return this.database.one('SELECT COUNT(*) FROM articles WHERE uuid = $[uuid]', { uuid: termToString(id) }, (data: { count: number }) => +data.count > 0);
+    return this.database.one('SELECT COUNT(*) FROM articles WHERE iri = $[iri]', { iri: id.value }, (data: { count: number }) => +data.count > 0);
   }
 
   async count(): Promise<number> {
@@ -69,13 +69,13 @@ export default class PostgresArticles implements Articles {
   }
 
   async* [Symbol.asyncIterator](): AsyncIterator<[NamedNode, DatasetCore]> {
-    yield* await this.database.any('SELECT uuid, article FROM articles').then((rows) => rows.map(async (row: { uuid: string; article: string }) => [stringToTerm(row.uuid), await fromStream(this.dataFactory.dataset(), this.parser.import(toReadableStream(row.article)))])) as [[NamedNode, DatasetCore]];
+    yield* await this.database.any('SELECT iri, article FROM articles').then((rows) => rows.map(async (row: { iri: string; article: string }) => [namedNode(row.iri), await fromStream(this.dataFactory.dataset(), this.parser.import(toReadableStream(row.article)))])) as [[NamedNode, DatasetCore]];
   }
 
   static async createTables(database: IBaseProtocol<IMain>): Promise<void> {
     await database.none('DROP TABLE IF EXISTS articles');
     await database.none(`CREATE TABLE IF NOT EXISTS articles (
-      uuid varchar (34) NOT NULL UNIQUE,
+      iri varchar (128) NOT NULL UNIQUE,
       article text NOT NULL,
       created time without time zone DEFAULT CURRENT_TIMESTAMP NOT NULL
     )`);
