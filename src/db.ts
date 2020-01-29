a305ba7a30c216a7dfbbbf7dff757b6093c85f25
Chris Wilkinson <c.wilkinson@elifesciences.org>
2020-01-29 20:30:46 +0000

    Simplify string to number coercion

--- src/db.ts
+++ src/db.ts
@@ -5,7 +5,7 @@ const config: Readonly<IConnectionParameters> = {
   password: process.env.DATABASE_PASSWORD,
   database: process.env.DATABASE_NAME,
   host: process.env.DATABASE_HOST,
-  port: process.env.DATABASE_PORT ? parseFloat(process.env.DATABASE_PORT) : undefined,
+  port: process.env.DATABASE_PORT ? +process.env.DATABASE_PORT : undefined,
 };
 
 export default config;
