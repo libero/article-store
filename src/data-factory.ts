9f9e78331ae2c0e90d64d080b4c58667e44b19dc
Chris Wilkinson <c.wilkinson@elifesciences.org>
2020-01-29 10:48:53 +0000

    refactor: use the same data factory inside the dataset factory
    Rather than using its own, make the dataset factory use the same data factory. It was actually using the same implementation, but it's now explicit.

--- src/data-factory.ts
+++ src/data-factory.ts
@@ -1,8 +1,12 @@
 import dataFactory from '@rdfjs/data-model';
 import createDataset from 'rdf-dataset-indexed';
+import { DatasetCore, Quad } from 'rdf-js';
 import { ExtendedDataFactory } from './middleware/dataset';
 
-const extendedDataFactory: ExtendedDataFactory = { ...dataFactory, dataset: createDataset };
+const extendedDataFactory: ExtendedDataFactory = {
+  ...dataFactory,
+  dataset: (quads?: Array<Quad>): DatasetCore => createDataset(quads, extendedDataFactory),
+};
 
 export const {
   dataset, blankNode, namedNode, literal, quad,
