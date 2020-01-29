4ae6bcf6f9145c76052697e8b1f693540980180e
Chris Wilkinson <c.wilkinson@elifesciences.org>
2020-01-29 16:18:08 +0000

    Missing port

--- src/db.ts
+++ src/db.ts
@@ -5,4 +5,5 @@ export default {
   password: process.env.DATABASE_PASSWORD,
   database: process.env.DATABASE_NAME,
   host: process.env.DATABASE_HOST,
+  port: process.env.DATABASE_PORT,
 } as IConnectionParameters;
