68693d509430ec4f6e29b5385314eaf604be4227
Chris Wilkinson <c.wilkinson@elifesciences.org>
2020-01-30 10:41:31 +0000

    Use base pg type to avoid weird import

--- src/db.ts
+++ src/db.ts
@@ -1,6 +1,6 @@
-import { IConnectionParameters } from 'pg-promise/typescript/pg-subset';
+import { ConnectionConfig } from 'pg';
 
-const config: Readonly<IConnectionParameters> = {
+const config: Readonly<ConnectionConfig> = {
   user: process.env.DATABASE_USER,
   password: process.env.DATABASE_PASSWORD,
   database: process.env.DATABASE_NAME,
